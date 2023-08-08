2ce87b18d2b248cc916bb9e964e1ef79cc23020b
Sahil Raj <98708331+codysahil@users.noreply.github.com>
2023-08-08 16:38:05 +0530

    Create node.cpp

---
+++ Find_n_by_kTh_node/node.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    // your code here
+    Node *temp = head;
+    Node *curr = head;
+    
+    int count = 0;
+    while(temp!=0){
+        count++;
+        temp = temp->next;
+    }
+    int count2 = 0;
+    while(curr!=0){
+        count2++;
+        if(count2 == count/k && count%k ==0){
+            return curr->data;
+        }
+        else if(count2 == count/k && count%k !=0 ){
+            return curr->next->data;
+        }
+        curr = curr->next;
+    }
+}
