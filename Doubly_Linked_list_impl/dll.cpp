18a770b4e767b936f5694b74b0036170e4458a4a
Sahil Raj <98708331+codysahil@users.noreply.github.com>
2023-08-09 17:37:35 +0530

    Create dll.cpp

---
+++ Doubly_Linked_list_impl/dll.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void display() const {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    ~DoublyLinkedList() {
+        Node* current = head;
+        while (current) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    DoublyLinkedList myList;
+    myList.push_back(10);
+    myList.push_back(20);
+    myList.push_back(30);
+
+    myList.display();
+
+    return 0;
+}
